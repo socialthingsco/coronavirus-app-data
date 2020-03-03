160720938f6fe473bae174b96aa27e909eae0436
Zachary Shakked <zach@shakd.io>
2020-03-03 17:25:53 -0500

    clean map file

---
+++ clean-map.js
@@ -0,0 +1,12 @@
+document.querySelector("#ember95 > margin-container > full-container > div > div.flex-horizontal.collapsed-bar-container.overflow-hidden > div:nth-child(3) > a").click();
+document.querySelector("#ember95 > margin-container > full-container > div > div.flex-horizontal.collapsed-bar-container.overflow-hidden > div:nth-child(3) > a").click();
+document.querySelector("#ember95 > margin-container > full-container > div > div.flex-horizontal.collapsed-bar-container.overflow-hidden > div:nth-child(3) > a").click();
+document.querySelector("#ember117").click()
+
+setTimeout(function() {
+    document.querySelector("#ember75 > div > div.flex-vertical.map-toolbar-right.no-pointer-events.flex-wrapping > nav").style.display = "none";
+    document.querySelector("#ember95 > margin-container").style.display = "none";
+    document.querySelector("#ember9").style.display = "none";
+    document.querySelector("#ember95").style.display = "none";
+    document.querySelector("#ember117").style.display = "none";
+}, 1000);
